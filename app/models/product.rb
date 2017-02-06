c63bb06856fc94dcc03968a8b4548bf7efdced00
Yuri <yuri.manahan@gmail.com>
2017-02-06 14:14:30 -0500

    displaying price in dollars

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
-validates :description, :name, presence: true
-validates :price_in_cents, numericality: {only_integer: true}
+  validates :description, :name, presence: true
+  validates :price_in_cents, numericality: {only_integer: true}
+
+  def formatted_price
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
 
 end

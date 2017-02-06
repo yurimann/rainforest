50a38ddd3e74bde412c49d8e04144067eee35516
Yuri <yuri.manahan@gmail.com>
2017-02-06 13:46:02 -0500

    Set up product controllers

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,48 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to_products_url
+      else
+        render :new
+      end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+      if @product.update_attributes(product_params)
+        redirect_to_products_url(@product)
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to_products_url
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'products/index'
+  # get 'products/show'
+  # get 'products/new'
+  # get 'products/edit'
+
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
